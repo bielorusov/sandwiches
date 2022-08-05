1eb2d26d08a7ff5480cb99f130b0e48627db3123
Dmytro Bielorusov <dmytro.bielorusov@gmail.com>
2022-08-05 14:29:55 +0300

    Initial Commit

---
+++ Sandwiches/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Sandwiches
+//
+//  Created by Dmitriy Belorusov on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
